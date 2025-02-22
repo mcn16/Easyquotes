253539b917037afa8a007d00bb1f9ca480ba0faa
mcn16 <miranda.c.negro@gmail.com>
2025-02-22 15:21:24 -0500

    update

--- Easyquotes.bat
+++ Easyquotes.bat
@@ -1,35 +1,35 @@
 @echo off
 title Quotes
+color 0A
+                                                                                                                                                                                                                               
 color 0A                                                                                                                                                                                                                            
 :menu
 echo Please choose an option:
 echo 1. Fetch text
-echo 2. Clear Screenshot folder
-echo 3. Exit
-set /p choice=Enter your choice (1,2 or 3):
-if "%choice%"=="1" goto run_script
-if "%choice%"=="2" goto clear
-if "%choice%"=="3" goto exit
 
+@@ -15,13 +14,13 @@ if "%choice%"=="3" goto exit
 
 
 :run_script
+python "C:\Users\miran\Desktop\Easyquotes\easyquotes.py" 
 python "C:\Users\miran\Desktop\Easyquotes\easyquotes.py"
 pause
 cls
 goto menu
 
 :clear
+del /q "C:\Users\miran\Pictures\Screenshots\*.*" 
 del /q "C:\Users\miran\Pictures\Screenshots\*.*"
 for /d %%p in ("C:\Users\miran\Pictures\Screenshots\*") do rd /s /q "%%p" > nul 2>&1 :: Also change to your screen shots folder
 echo Cleared!
 pause
-cls
-goto menu
 
+@@ -31,7 +30,7 @@ goto menu
 
 :exit
 echo Exiting...
+pause
+
 
 
 
