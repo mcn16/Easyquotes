43efde8e7e4535d1ca64b7122edcf447f64bb7ea
mcn16 <miranda.c.negro@gmail.com>
2025-02-22 15:23:50 -0500

    chill

--- Easyquotes.bat
+++ Easyquotes.bat
@@ -2,7 +2,10 @@
 title Quotes
 color 0A
                                                                                                                                                                                                                                
+<<<<<<< HEAD
 color 0A                                                                                                                                                                                                                            
+=======
+>>>>>>> parent of d33e04e (added automatic copy and paste of the text)
 :menu
 echo Please choose an option:
 echo 1. Fetch text
@@ -12,14 +15,20 @@ echo 1. Fetch text
 
 :run_script
 python "C:\Users\miran\Desktop\Easyquotes\easyquotes.py" 
+<<<<<<< HEAD
 python "C:\Users\miran\Desktop\Easyquotes\easyquotes.py"
+=======
+>>>>>>> parent of d33e04e (added automatic copy and paste of the text)
 pause
 cls
 goto menu
 
 :clear
 del /q "C:\Users\miran\Pictures\Screenshots\*.*" 
+<<<<<<< HEAD
 del /q "C:\Users\miran\Pictures\Screenshots\*.*"
+=======
+>>>>>>> parent of d33e04e (added automatic copy and paste of the text)
 for /d %%p in ("C:\Users\miran\Pictures\Screenshots\*") do rd /s /q "%%p" > nul 2>&1 :: Also change to your screen shots folder
 echo Cleared!
 pause
@@ -29,8 +38,11 @@ pause
 :exit
 echo Exiting...
 pause
+<<<<<<< HEAD
 
 
+=======
+>>>>>>> parent of d33e04e (added automatic copy and paste of the text)
 
 
 
